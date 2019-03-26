5af9db2c347df277628159274755596fef79755a
bananajam98 <30919938+bananajam98@users.noreply.github.com>
2019-03-26 12:45:51 -0500

    Create test.py

---
+++ code/test.py
@@ -0,0 +1,30 @@
+
+abc=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+number=[]
+
+#word input
+print("Print word (in lowercase and spaced out EX: 't e s t')")
+word = input().split()
+
+
+#key input
+print('key(1-25)')
+key = input()
+
+#finding index
+#for x in word:
+ #   if (word.index(x) == abc.index(word.index(x))):
+#        number.appended(abc.index()
+
+
+
+
+
+print("word length")
+print(word.__len__())
+print("word")
+print(word)
+
+
+print(word.index(1))
+
