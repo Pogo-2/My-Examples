8a3e5fbe1a5c519ec4219cc98c17b8ded59c3655
bananajam98 <30919938+bananajam98@users.noreply.github.com>
2019-03-27 12:21:55 -0500

    final
    we done here

--- code/Cypher.py
+++ code/Cypher.py
@@ -1,6 +1,3 @@
-
-
-
 debug = False
 
 abc=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
@@ -35,8 +32,26 @@ if (yorn == 0):
     for x in edit:
         final.append(abc[x])
 else:
-
-
+    #word input
+    print("Print word (in lowercase and spaced out EX: 't e s t')")
+    word = input().split()
+    #key input
+    print('key(1-25)')
+    key = int(input())
+    for x in word:
+        for y in abc:
+            if(x == y):
+                hold.append(abc.index(y))
+    #math
+    for x in hold:
+        x = x - key
+        if(x < 0):
+            x = x + 25
+            edit.append(x)
+        else: edit.append(x)
+    #convert back to text
+    for x in edit:
+        final.append(abc[x])
 
 
 
