fcfc45fe804709f5316d26c4c158c9bd26b4535f
Pogo-2 <germanyben64@gmail.com>
2020-01-09 22:46:31 +0000

    created a notification app/ added weather grabbing feature

---
+++ updated_code/Notify_me/get_weather.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3 
+import requests
+import json
+import os
+
+
+print("starting script ..")
+
+#API variables
+key = "f4e0158f7c5c7eead5286288b221e1bd"
+latitude = "32.6960"
+longitude = "-97.6022"
+
+url = f"https://api.darksky.net/forecast/{key}/{latitude},{longitude}?exclude=minutely,hourly,daily,alerts,flags"
+
+data = requests.get(url, json={"key": "value"})
+content = json.dumps((data.json()), sort_keys=True, indent=4)
+
+os.system("touch Weather.json")
+with open("./Weather.json","w") as fp:
+    fp.write(content)
+
+
+
+#Powered by Dark Sky API
